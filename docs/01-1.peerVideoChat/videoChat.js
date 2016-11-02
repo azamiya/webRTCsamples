3da4dddee182c488f317a9bb51bf4a187c04bac5
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-11-02 19:34:41 +0900

    fix value

--- docs/01-1.peerVideoChat/videoChat.js
+++ docs/01-1.peerVideoChat/videoChat.js
@@ -1,25 +1,20 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
  
-var localStream;    // 自分の映像ストリームを保存しておく変数
-var connectedCall;  // 接続したコールを保存しておく変数
+var localStream;
+var connectedCall;
  
 var peer = new Peer({ key: 'px6645j12izilik9', debug: 3});
 
 window.onload = function(){
     displayMyCamera();
+    getMyID();
 }
 
-//Get peer id
-peer.on('open', function(){
-    connectedCall = call;
-    document.getElementById("my-id").innerHTML = peer.id;
-    call.answer(localStream);
-
-    call.on('stream', function(stream){
-        var url = URL.createObjectURL(stream);
-        document.getElementById("peer-video").src = url;
+function getMyID() {
+    peer.on('open', function(){
+        document.getElementById("my-id").innerHTML = peer.id;
     });
-});
+}
 
 //take calling from other peer
 peer.on('call', function(call){
@@ -33,25 +28,6 @@ peer.on('call', function(call){
     });
 });
  
-/*$(function() {
- 
-    //recieve calling from other peer
-    $('#call-start').click(function(){
-        var peer_id = $('#peer-id-input').val();
-        var call = peer.call(peer_id, localStream);
-            
-        call.on('stream', function(stream){
-            $("#peer-id").text(call.peer);
-            var url = URL.createObjectURL(stream);
-            $('#peer-video').prop('src', url);
-        });
-    });
- 
-    // End　Callボタンクリック時の動作
-    $('#call-end').click(function(){
-        connectedCall.close();
-    });
-});*/
 
 function displayMyCamera(){
     navigator.getUserMedia({audio: true, video: true}, function(stream){
