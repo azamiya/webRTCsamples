37d1a49e73f933cbf361ba1e2bab03ece5dbfe8a
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-11-02 20:17:25 +0900

    add skyway videochat sample

--- docs/02-1.skywayVideoChat/videoChat.js
+++ docs/02-1.skywayVideoChat/videoChat.js
@@ -3,7 +3,7 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
 var localStream;
 var connectedCall;
  
-var peer = new Peer({ key: '26f45df9-8480-4088-9ae2-8daec97a0a0a', debug: 3});
+var peer = new Peer({ key: '5bea388b-3f95-4e1e-acb5-a34efdd0c480', debug: 3});
 
 window.onload = function(){
     displayMyCamera();
