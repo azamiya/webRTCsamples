193713d08094fb20fbc1bb50a055d3afe9d832ce
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-11-02 20:33:40 +0900

    change API key

--- docs/02-1.skywayVideoChat/videoChat.js
+++ docs/02-1.skywayVideoChat/videoChat.js
@@ -3,7 +3,11 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
 var localStream;
 var connectedCall;
  
-var peer = new Peer({ key: 'bc3035be-047d-4b36-bab7-bd2a8b0c7b33', debug: 3});
+var peer = new Peer({
+    key: 'bc3035be-047d-4b36-bab7-bd2a8b0c7b33',
+    debug: 3,
+    turn : false
+});
 
 window.onload = function(){
     displayMyCamera();
